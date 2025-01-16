ad9586669486bf73f0450d5565d3e5e5aaa9b485
Tushar Lohar <tusharlohar1137@gmail.com>
2025-01-16 20:29:26 +0530

    update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("main-navbar");
+    const offset = navbar.offsetTop; 
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > offset) {
+            navbar.classList.add("fixed");
+        } else {
+            navbar.classList.remove("fixed");
+        }
+    });
+});
