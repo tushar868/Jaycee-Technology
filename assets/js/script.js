6eddf9101040c24353eccaa7cbc71926d8b0afc6
Tushar Lohar <tusharlohar1137@gmail.com>
2025-02-06 13:14:16 +0530

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+/**
+   * Back to top button
+   */
+let backtotop = document.querySelector('.back-to-top');
+
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active');
+    } else {
+      backtotop.classList.remove('active');
+    }
+  };
+
+  window.addEventListener('load', toggleBacktotop);
+  window.addEventListener('scroll', toggleBacktotop);
+
+  backtotop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+}
+
+
+/**
+   * Preloader
+   */
+let preloader = document.querySelector('#preloader');
+
+if (preloader) {
+  window.addEventListener('load', () => {
+    preloader.style.display = 'none';
+  });
+}
+
+
+/**
+   * Animation on scroll
+   */
+window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+
+  
